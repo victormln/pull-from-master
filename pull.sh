ae7dc4077e5b56afa28b27a00b059124023fc275
victormln <victor.molinaf@gmail.com>
2019-11-25 22:55:52 +0100

    Lint bash and some other languague fixes

--- pull.sh
+++ pull.sh
@@ -11,24 +11,22 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 ORANGE='\033[0;33m'
 CYAN='\033[0;36m'
-ACTUAL_DIR=$( dirname "${BASH_SOURCE[0]}" )
+ACTUAL_DIR=$(dirname "${BASH_SOURCE[0]}")
 IS_ABSOLUTE_PATH="true"
 HAS_UNCOMMITED_FILES=0
 CURRENT_BRANCH="master"
 
-function is_an_absolute_path {
+function is_an_absolute_path() {
   if [[ $1 == /* ]]; then IS_ABSOLUTE_PATH="true"; else IS_ABSOLUTE_PATH="false"; fi
 }
 
-function pull_from_master {
+function pull_from_master() {
   has_uncommited_files
-  if [ $HAS_UNCOMMITED_FILES -ne 0 ]
-  then
+  if [ $HAS_UNCOMMITED_FILES -ne 0 ]; then
     git stash
   fi
   get_current_branch
-  if [ $CURRENT_BRANCH == "master" ]
-  then
+  if [ $CURRENT_BRANCH == "master" ]; then
     git pull origin master
   else
     get_current_branch
@@ -36,46 +34,43 @@ function pull_from_master {
     git pull origin master
     git checkout $CURRENT_BRANCH
   fi
-  if [ $HAS_UNCOMMITED_FILES -ne 0 ]
-  then
-    git stash pop > /dev/null 2>&1
+  if [ $HAS_UNCOMMITED_FILES -ne 0 ]; then
+    git stash pop >/dev/null 2>&1
   fi
   echo "==========================================="
 }
 
-function has_uncommited_files {
+function has_uncommited_files() {
   git diff-index --quiet HEAD --
   HAS_UNCOMMITED_FILES=$(echo $?)
 }
 
-function get_current_branch {
+function get_current_branch() {
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 }
 
-function is_a_git_repository {
-  cd "$1" > /dev/null 2>&1 || exit
-  if [ ! -e "$1/.git" ];
-  then
+function is_a_git_repository() {
+  cd "$1" >/dev/null 2>&1 || exit
+  if [ ! -e "$1/.git" ]; then
     echo -e "${ERROR}[ERROR]${NC} The directory: $1 its not a git repository"
   else
-    echo -e "${OK}Actualizando: $1${NC}"
+    echo -e "${OK}Pulling: $1${NC}"
     echo "==========================================="
     pull_from_master $1
   fi
   cd "$ACTUAL_DIR" || exit
 }
 
-function get_all_paths {
-  while read REPOSITORYPATH
-  do
-      is_an_absolute_path $REPOSITORYPATH
-      if [ $IS_ABSOLUTE_PATH == "true" ]
-      then
-        is_a_git_repository "$REPOSITORYPATH"
-      else
-        echo -e "${ERROR}[ERROR]${NC} The directory: $REPOSITORYPATH its not an absolute path"
-      fi
-  done < $( dirname "${BASH_SOURCE[0]}" )/repositories.txt
+function get_all_paths() {
+  input=$(dirname "${BASH_SOURCE[0]}")/repositories.txt
+  while IFS= read -r repository_path; do
+    is_an_absolute_path "$repository_path"
+    if [ $IS_ABSOLUTE_PATH == "true" ]; then
+      is_a_git_repository "$repository_path"
+    else
+      echo -e "${ERROR}[ERROR]${NC} The directory: $repository_path its not an absolute path"
+    fi
+  done <"$input"
 }
 
 get_all_paths
